241f9ef5f97d9d30955574100f1535d88d86425b
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-11-24 18:40:03 -0800

    Updated db schema
    Changed primary key to a serialized integer. Decided this would be safer than relying upon the imdb_id (as this, unlikely as it is, may change). Created an imdb_id column, which I then indexed for faster lookup.
    
    Added additional columns for all the info I would like to display about a film. Once a user rates the film and it is added to the database, the next time the film details are requested, an api call will not need to be made

--- backend/film-db-schema.sql
+++ backend/film-db-schema.sql
@@ -1,9 +1,16 @@
 CREATE TABLE films (
-  imdb_id INTEGER PRIMARY KEY,
+  id SERIAL INTEGER PRIMARY KEY,
+  imdb_id TEXT NOT NULL 
   title TEXT NOT NULL,
+  director TEXT,
   release_year INTEGER,
   description TEXT,
+  genre TEXT,
+  rating TEXT,
+  runtime TEXT,
   logo_url TEXT,
   thumbs_up INTEGER DEFAULT 0,
   thumbs_down INTEGER DEFAULT 0
 );
+
+CREATE INDEX imdb_id_idx ON films (imdb_id);
