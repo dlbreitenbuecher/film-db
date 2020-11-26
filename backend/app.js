d97e6a17f314204a5e5312ca328a28ad3cc414f7
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-11-25 17:43:15 -0800

    Made some minor style changes for consistency
    double quotes are now single quotes

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@
 
 /** Express app for Film-DB */
 
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { NotFoundError } = require("./expressError");
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const { NotFoundError } = require('./expressError');
 const filmsRoutes = require('./routes/films');
 
 
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
 app.use('/films', filmsRoutes);
 
@@ -26,7 +26,7 @@ app.use(function (req, res, next) {
 
 /** Generic error handler; anything unhandled goes here. */
 app.use(function (err, req, res, next) {
-  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  if (process.env.NODE_ENV !== 'test') console.error(err.stack);
   const status = err.status || 500;
   const message = err.message;
 
--- backend/config.test.js
+++ backend/config.test.js
@@ -1,25 +1,25 @@
-"use strict";
+'use strict';
 
-describe("config can come from env", function () {
-  test("works", function() {
-    process.env.API_SECRET_KEY = "abc";
-    process.env.PORT = "5000";
-    process.env.DATABASE_URL = "other";
-    process.env.NODE_ENV = "other";
+describe('config can come from env', function () {
+  test('works', function() {
+    process.env.API_SECRET_KEY = 'abc';
+    process.env.PORT = '5000';
+    process.env.DATABASE_URL = 'other';
+    process.env.NODE_ENV = 'other';
 
-    const config = require("./config");
+    const config = require('./config');
     
-    expect(config.API_SECRET_KEY).toEqual("abc");
+    expect(config.API_SECRET_KEY).toEqual('abc');
     expect(config.PORT).toEqual(5000);
-    expect(config.getDatabaseUri()).toEqual("other");
+    expect(config.getDatabaseUri()).toEqual('other');
 
     delete process.env.SECRET_KEY;
     delete process.env.PORT;
     delete process.env.DATABASE_URL;
 
-    expect(config.getDatabaseUri()).toEqual("film_db");
-    process.env.NODE_ENV = "test";
+    expect(config.getDatabaseUri()).toEqual('film_db');
+    process.env.NODE_ENV = 'test';
 
-    expect(config.getDatabaseUri()).toEqual("film_db_test");
+    expect(config.getDatabaseUri()).toEqual('film_db_test');
   });
 })
