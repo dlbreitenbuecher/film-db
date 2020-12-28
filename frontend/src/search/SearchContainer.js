d92161b878403b76f27f76b1119d89e3969e215e
David Breitenbuecher <dlbreitenbuecher@gmail.com>
2020-12-28 00:11:09 -0800

    Address errors for Netlify deploy

--- frontend/src/search/SearchContainer.js
+++ frontend/src/search/SearchContainer.js
@@ -51,7 +51,7 @@ function SearchContainer() {
 
     fetchFilms();
   },
-    [searchTerm]
+    [searchTerm, searchResults]
   );
 
 
--- frontend/src/search/SearchResultList.js
+++ frontend/src/search/SearchResultList.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SearchResultCard from './SearchResultCard';
 import { Typography, Box } from '@material-ui/core';
 
